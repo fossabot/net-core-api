90e82998017864510156328b9cff0c272d201aa5
George Theofilis <theofilis.g@gmail.com>
2018-05-16 20:34:48 +0300

    Init Project

---
+++ dotnet-hello-world-web/Program.cs
@@ -0,0 +1,44 @@
+﻿using App.Metrics;
+using App.Metrics.AspNetCore;
+using App.Metrics.Formatters;
+using App.Metrics.Formatters.Prometheus;
+
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace RestApi
+{
+    public class Program
+    {
+        private static IMetricsRoot Metrics { get; set; }
+
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        private static IWebHost BuildWebHost(string[] args)
+        {
+            Metrics = AppMetrics.CreateDefaultBuilder()
+                .OutputMetrics.AsPrometheusPlainText()
+                .OutputMetrics.AsPrometheusProtobuf()
+                .Build();
+            
+            return WebHost.CreateDefaultBuilder(args)
+                .ConfigureMetrics(Metrics)
+                .UseMetrics(
+                    options =>
+                    {
+                        options.EndpointOptions = endpointsOptions =>
+                        {
+                            endpointsOptions.MetricsTextEndpointOutputFormatter =
+                                Metrics.OutputMetricsFormatters.GetType<MetricsPrometheusTextOutputFormatter>();
+                            endpointsOptions.MetricsEndpointOutputFormatter = 
+                                Metrics.OutputMetricsFormatters.GetType<MetricsPrometheusProtobufOutputFormatter>();
+                        };
+                    })
+                .UseStartup<Startup>()
+                .Build();            
+        }
+    }
+}
---
+++ dotnet-hello-world-web/Startup.cs
@@ -0,0 +1,44 @@
+﻿using App.Metrics;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RestApi
+{
+    public class Startup
+    {        
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var metrics = AppMetrics.CreateDefaultBuilder()
+                .OutputMetrics.AsPrometheusPlainText()
+                .OutputMetrics.AsPrometheusProtobuf()
+                .Build();
+
+            services.AddMetrics(metrics);
+            services.AddMetricsTrackingMiddleware();
+            
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMetricsAllMiddleware();
+            app.UseMvc();
+        }
+    }
+}
